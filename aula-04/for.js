f898d0e7b6fc8c69b3937dd25eff7ae01d91bf2d
Cristian Araujo <araujocristian@live.com>
2021-11-05 21:36:32 -0300

    for

--- aula-04/for.js
+++ aula-04/for.js
@@ -1,4 +1,6 @@
-const arr = [10, 20, 30, 40, 50];
+const prompt = require("prompt-sync")({ sigint: true });
+
+// const arr = [10, 20, 30, 40, 50];
 
 // for-in
 // for (index in arr) {
@@ -14,6 +16,49 @@ const arr = [10, 20, 30, 40, 50];
 //   console.log(arr[index]);
 // }
 
-for (let vez = 1; vez <= 10; vez++) {
-  console.log("Olá, pela " + vez + "º vez");
+// for (let vez = 1; vez <= 10; vez++) {
+//   console.log("Olá, pela " + vez + "º vez");
+// }
+
+// Exemplo rolagem de dados
+
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+function rolar(dado) {
+  return randomIntFromInterval(1, dado);
+}
+
+function splitInput(input) {
+  const splitedInput = input.toUpperCase().split("D");
+  const normalizedInput = [
+    parseInt(splitedInput[0]),
+    parseInt(splitedInput[1]),
+  ];
+  return normalizedInput;
+}
+
+const input = prompt("Digite a quantidade e o dado no formato XDY, ex. 2D6: ");
+const quantidadeEDado = splitInput(input);
+const quantidade = quantidadeEDado[0];
+const dado = quantidadeEDado[1];
+
+let jogadasDoDado = [];
+let total = 0;
+
+for (let i = 0; i < quantidade; i++) {
+  const resultado = rolar(dado);
+  jogadasDoDado.push(resultado);
+  total = total + resultado;
+}
+
+console.log("Rolagens: " + jogadasDoDado + " Total: " + total);
+
+/**
+ * 
+Crie um código que leia 10 valores inteiros e:
+• Encontre e mostre o maior valor
+• Encontre e mostre o menor valor
+• Calcule e mostre a média dos números lidos
+ */
